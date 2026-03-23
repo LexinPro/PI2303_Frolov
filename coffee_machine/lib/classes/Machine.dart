1caa5cd84d758440f8002f08ab13891097b5f0f9
LexinPro <frolovu301@gmail.com>
2026-03-23 12:43:00 +0300

    Создание класса Machine с закрытыми полями

---
+++ coffee_machine/lib/classes/Machine.dart
@@ -0,0 +1,27 @@
+class Machine {
+    int _coffeeBeans = 0;
+    int _milk = 0;
+    int _water = 0;
+
+    int get coffeeBeans => _coffeeBeans;
+    int get milk => _milk;
+    int get water => _water;
+
+    set coffeeBeans(int coffeeBeans) {
+        if (coffeeBeans >= 0) {
+            _coffeeBeans = coffeeBeans;
+        }
+    }
+
+    set milk(int milk) {
+        if (milk >= 0) {
+            _milk = milk;
+        }
+    }
+
+    set water(int water) {
+        if (water >= 0) {
+            _water = water;
+        }
+    }
+}
