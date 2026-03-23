de922239d6b6cbf67bd95845d68ae5976925e5d0
LexinPro <frolovu301@gmail.com>
2026-03-23 12:56:05 +0300

    add: Инициализирована точка входа

--- coffee_machine/lib/coffee_machine.dart
+++ coffee_machine/lib/coffee_machine.dart
@@ -1,3 +1,11 @@
-int calculate() {
-  return 6 * 7;
+import "./classes/Machine.dart";
+
+
+void main() {
+    Machine machine = Machine();
+
+    print("Команды:");
+    print("1 - приготовить эспрессо");
+    print("2 - добавить ресурсы");
+    print("3 - выход");
 }
