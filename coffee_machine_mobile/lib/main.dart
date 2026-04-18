f6d02ccded363f3dbf94692b02c82c02d1eed5b3
LexinPro <frolovu301@gmail.com>
2026-04-19 00:03:48 +0300

    add: Инициализация проекта

---
+++ coffee_machine_mobile/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coffee Machine',
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF795548),
+          title: Text("Coffee Machine"),
+          foregroundColor: Colors.white,
+        ),
+      )
+    );
+  }
+}
