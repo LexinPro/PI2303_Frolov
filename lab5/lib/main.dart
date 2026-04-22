d6caa45a6063a42e4052a84437f0f8045f31adfb
LexinPro <frolovu301@gmail.com>
2026-04-22 23:03:23 +0300

    add: Реализованные списки собраны в приложение

--- lab5/lib/main.dart
+++ lab5/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lab5/infinity_list.dart';
+import 'package:lab5/infinity_math_list.dart';
+import 'package:lab5/simple_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Списки',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.green),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Список элементов'),
     );
   }
 }
@@ -27,37 +30,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: "Простой список",),
+              Tab(text: "Номер строк",),
+              Tab(text: "Степени двойки",),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        body: TabBarView(
+          children: <Widget>[
+            SimpleList(),
+            InfinityList(),
+            InfinityMathList(),
+          ],
+        )
       ),
     );
   }
