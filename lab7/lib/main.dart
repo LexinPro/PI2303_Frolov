b0f02b285d08f72cb0735c1b15093c7b82d2412e
LexinPro <frolovu301@gmail.com>
2026-04-23 21:19:56 +0300

    add: Основной экран вставлен в приложение

--- lab7/lib/main.dart
+++ lab7/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab7/screens/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      initialRoute: '/',
-      routes: {
-
-      },
+      home: MainScreen(),
     );
   }
 }
