7ec1d6c80f896eff82286eebb2cd46ed0d26f86b
LexinPro <frolovu301@gmail.com>
2026-04-26 14:19:17 +0300

    add: Все реализованные классы добалвены в точку входа приложения

--- lab8_2/lib/main.dart
+++ lab8_2/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lab8_2/widgets/my_appbar.dart';
+import 'package:lab8_2/widgets/news_list.dart';
 
 
 void main() => runApp(MyApp());
@@ -11,7 +13,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Лента КубГАУ",
-      home: Column(),
+      home: Scaffold(
+        appBar: MyAppbar(title: "Новостная лента КубГАУ"),
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: NewsList(),
+        ),
+      ),
     );
   }
 }
